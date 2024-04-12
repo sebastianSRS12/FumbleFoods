e82d68bebfb3cb069c37e9b1a2fa232991cb9b20
Tyson Veik <111925371+TysonVeik@users.noreply.github.com>
2024-04-12 10:29:05 -0500

    Update User.cs

--- src/User.cs
+++ src/User.cs
@@ -8,8 +8,9 @@ namespace FumbleFoods.src
 {
     public class User
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
